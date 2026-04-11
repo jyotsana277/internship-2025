03e85bbddd5a899dc63d3bb1b06486d2155ca234
Jyotsana <jyotsanasivaram444@gmail.com>
2026-04-11 13:56:45 +0530

    Delete elasticsearch directory

--- elasticsearch/search_data.py
+++
@@ -1,8 +0,0 @@
-from elasticsearch import Elasticsearch
-
-es = Elasticsearch("http://localhost:9200")
-
-res = es.search(index="my-sparse-index", size=10)
-
-for hit in res["hits"]["hits"]:
-    print(hit["_source"])
